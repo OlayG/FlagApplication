9eae39539e29f0546b395639a0a331aad014e404
OlayG <ogureje@yahoo.com>
2019-03-07 11:44:18 -0500

    Updated code with comments to breakdown almost everything being done

--- app/src/main/java/com/noname/flagapplication/FlagColorFragment.java
+++ app/src/main/java/com/noname/flagapplication/FlagColorFragment.java
@@ -13,19 +13,34 @@ import android.view.ViewGroup;
  * A simple {@link Fragment} subclass.
  */
 public class FlagColorFragment extends Fragment {
-    public static final String BACKGROUND_COLOR = "backgroundColor";
+    // Here is a constant used as the key for my Bundle
+    public static final String BACKGROUND_COLOR = "BACKGROUND_COLOR";
+
+    // Here i declare my variables that will be used in this fragment
     private View background;
     private int backgroundColor;
 
+    // Empty constructor
     public FlagColorFragment() {
         // Required empty public constructor
     }
 
-    public static FlagColorFragment newInstance(int color) {
+    // This is a way to create a fragment adding a default color as parameter
+    public static FlagColorFragment newInstance(int defaultColor) {
+        // Create a fragment variable and initialize it
         FlagColorFragment flagColorFragment = new FlagColorFragment();
+
+        // Create a Bundle variable and initialize it
         Bundle args = new Bundle();
-        args.putInt(BACKGROUND_COLOR, color);
+
+        // using my bundle variable to save my default color
+        // Notice the key here is the constant from above
+        args.putInt(BACKGROUND_COLOR, defaultColor);
+
+        // Here I set my bundle variable to the fragment
         flagColorFragment.setArguments(args);
+
+        // Now I return the fragment
         return flagColorFragment;
     }
 
@@ -36,25 +51,36 @@ public class FlagColorFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_flag_color, container, false);
         if (savedInstanceState != null) {
-            // Restore last state
+            // Restore last state if saveInstanceState is not null
             backgroundColor = savedInstanceState.getInt(BACKGROUND_COLOR);
         } else {
+            // if we land here saveInstanceState was null so this means we are creating a new fragment from that newInstance() Method
+            // getArguments().getInt <-- this gets the bundle arguments from the newInstance() method above
             backgroundColor = getArguments().getInt(BACKGROUND_COLOR, 0);
         }
 
+        // Here I am initializing my view from the xml
         background = view.findViewById(R.id.view_background);
+        // This public method changes the background color
         changeBackgroundColor(backgroundColor);
+
+        // return the view as we are done with the initial setup
         return view;
     }
 
+    // I override this method so i can save the current background color in event of orientation change
     @Override
     public void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
+        // Here I use the paramater from the method to save the backgroundColor
         outState.putInt(BACKGROUND_COLOR, backgroundColor);
     }
 
+    // This is a public method that can be accessed from the activity to change the color
     public void changeBackgroundColor(int color) {
+        // Here i am setting the color to my backgroundColor variable which is scoped to this fragment
         backgroundColor = color;
+        // Here is the code that actually sets the backgroundColor
         background.setBackgroundColor(backgroundColor);
     }
 
--- app/src/main/java/com/noname/flagapplication/MainActivity.java
+++ app/src/main/java/com/noname/flagapplication/MainActivity.java
@@ -9,21 +9,29 @@ import android.widget.FrameLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    // Here are my constants to keep track of my fragments
     public static final String FRAG_TOP = "FRAG_TOP";
     public static final String FRAG_MIDDLE = "FRAG_MIDDLE";
     public static final String FRAG_BOTTOM = "FRAG_BOTTOM";
 
+    // Here I declare variables for my FrameLayout (this is where my fragments will be loaded too)
     FrameLayout top, middle, bottom;
+    // Here I declare my variables for FlagColorFragment (this is my fragments)
     FlagColorFragment fragTop, fragMiddle, fragBottom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Set my layout to this activity
         setContentView(R.layout.activity_main);
+        // initialize my FrameLayout variables here
         top = findViewById(R.id.top);
         middle = findViewById(R.id.middle);
         bottom = findViewById(R.id.bottom);
 
+        // This is for orientation changes
+        // If my savedInstanceState == null (this means I can create a new Fragment because this is a new entry into the app)
+        // else (this means I did create the fragments I just need to get them back
         if (savedInstanceState == null) {
             fragTop = FlagColorFragment.newInstance(R.color.colorPrimary);
             getSupportFragmentManager().beginTransaction().add(R.id.top, fragTop, FRAG_TOP).commit();
@@ -38,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
+        // This is a inline way for onClick (in here I handle the color changes and orientation changes)
         top.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -61,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
         bottom.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                // TODO: 3/7/2019 create code for 3rd country try to make it work
             }
         });
     }
